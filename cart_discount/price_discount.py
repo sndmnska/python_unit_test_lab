427d459e6e786acb5ed2619fb585dec1a5f844a6
sndmnska <102878282+sndmnska@users.noreply.github.com>
2024-01-31 12:52:19 -0600

    Create discount program, add test conditions

--- cart_discount/price_discount.py
+++ cart_discount/price_discount.py
@@ -9,9 +9,21 @@ def discount(item_prices):
     If a customer has ordered more than three items, the cheapest item is free.
     Example: if this function is called with a list of [10, 4, 20] then return 4.
     """
+    # Check for number of items
+    if len(item_prices) < 3:
+        return 0
+    else:
+        # If more than 3 items, return lowest priced item. 
+        # Replace lowest_price variable with lowest number as you iterate through the list
+        lowest_price = item_prices[0]
+        for item_price in item_prices:
+            if item_price < lowest_price:
+                lowest_price = item_price
+        return lowest_price
 
-    pass  # todo replace this line with your code 
 
+
+    
 
 if __name__ == '__main__':
     main()
--- cart_discount/test_discount.py
+++ cart_discount/test_discount.py
@@ -12,6 +12,34 @@ class TestDiscount(TestCase):
     
     # TODO more unit tests here. Each test should test one scenario
 
+    # test with a list of 2 prices
+    def test_list_of_two_prices(self):
+        prices = [15, 7]
+        expected_discount = 0
+        self.assertEqual(expected_discount, discount(prices))
+    
+
+    # test with a list 1 price
+        
+    def test_list_of_one_price(self):
+        prices = [15]
+        expected_discount = 0
+        self.assertEqual(expected_discount, discount(prices))
+    
+
+    # a list of 6 prices ?
+    
+    def test_list_of_six_prices(self):
+        prices = [1, 2.62, 3, 4, 5, 6]
+        expected_discount = 1
+        self.assertEqual(expected_discount, discount(prices))
+    
+
+    # what other data might this function have to deal with?
+        # Probably floating point numbers, though error handling for non-number strings would be good. 
+    
+    # remember a function has no control over how it is used
+    # it may be called with any data - and it should be able to handle whatever data it gets appropriately 
 
 if __name__ == '__main__':
     unittest.main()
